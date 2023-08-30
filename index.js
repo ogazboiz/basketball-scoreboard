35a34beac60d4284b544e2295c8ba2b8993887a9
Akpolo Ogagaoghene Prince <104951733+ogazboiz@users.noreply.github.com>
2023-08-31 00:28:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+homeScore= document.getElementById("home_score") 
+guestScore= document.getElementById("guest_score")
+resetScore= document.getElementById("reset")
+count = 0
+function home_one(){
+    count = count + 1
+    homeScore.innerText = count
+}
+function home_two(){
+    count = count + 2
+    homeScore.innerText = count
+}
+function home_three(){
+    count = count + 3
+    homeScore.innerText = count
+}
+function guest_one(){
+    count = count + 1
+    guestScore.innerText = count
+}
+function guest_two(){
+    count = count + 2
+    guestScore.innerText = count
+}
+function guest_three(){
+    count = count + 3
+    guestScore.innerText = count
+}
+function defaultss() {
+    count = 0
+    guestScore.innerText = count
+    homeScore.innerText = count
+}
