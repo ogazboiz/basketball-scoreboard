4db8dd508f60d22e8605880b9ef40c43c5b8c3f4
Akpolo Ogagaoghene Prince <104951733+ogazboiz@users.noreply.github.com>
2023-08-31 10:04:17 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
 // index.js
 
+let homeScore, guestScore, resetScore, homeCount, guestCount;
+
 document.addEventListener("DOMContentLoaded", function() {
-    let homeScore = document.getElementById("home_score");
-    let guestScore = document.getElementById("guest_score");
-    let resetScore = document.getElementById("reset");
-    let homeCount = 0;
-    let guestCount = 0;
-
-    function homeOne() {
-        homeCount += 1;
-        homeScore.textContent = homeCount;
-    }
-
-    function homeTwo() {
-        homeCount += 2;
-        homeScore.textContent = homeCount;
-    }
-
-    function homeThree() {
-        homeCount += 3;
-        homeScore.textContent = homeCount;
-    }
-
-    function guestOne() {
-        guestCount += 1;
-        guestScore.textContent = guestCount;
-    }
-
-    function guestTwo() {
-        guestCount += 2;
-        guestScore.textContent = guestCount;
-    }
-
-    function guestThree() {
-        guestCount += 3;
-        guestScore.textContent = guestCount;
-    }
-
-    function defaultss() {
-        homeCount = 0;
-        guestCount = 0;
-        homeScore.textContent = homeCount;
-        guestScore.textContent = guestCount;
-    }
+    homeScore = document.getElementById("home_score");
+    guestScore = document.getElementById("guest_score");
+    resetScore = document.getElementById("reset");
+    homeCount = 0;
+    guestCount = 0;
 });
+
+function homeOne() {
+    homeCount += 1;
+    homeScore.textContent = homeCount;
+}
+
+function homeTwo() {
+    homeCount += 2;
+    homeScore.textContent = homeCount;
+}
+
+function homeThree() {
+    homeCount += 3;
+    homeScore.textContent = homeCount;
+}
+
+function guestOne() {
+    guestCount += 1;
+    guestScore.textContent = guestCount;
+}
+
+function guestTwo() {
+    guestCount += 2;
+    guestScore.textContent = guestCount;
+}
+
+function guestThree() {
+    guestCount += 3;
+    guestScore.textContent = guestCount;
+}
+
+function defaultss() {
+    homeCount = 0;
+    guestCount = 0;
+    homeScore.textContent = homeCount;
+    guestScore.textContent = guestCount;
+}
