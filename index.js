43cb1e585f9ba604c1fef5d4859ef3353b662d50
Akpolo Ogagaoghene Prince <104951733+ogazboiz@users.noreply.github.com>
2023-08-31 09:53:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-let homeScore = document.getElementById("home_score") 
-let guestScore = document.getElementById("guest_score")
-let resetScore = document.getElementById("reset")
-let homeCount = 0
-let guestCount = 0
-function homeOne() {
-    homeCount   +=  1
-    homeScore.textContent= homeCount 
-}
-function homeTwo() {
-    homeCount   +=  2
-    homeScore.textContent = homeCount 
-}
-function homeThree() {
-    homeCount   +=  3
-    homeScore.textContent = homeCount 
-}
-function guestOne() {
-    guestCount  +=  1
-    guestScore.textContent = guestCount
-}
-function guestTwo() {
-    guestCount  +=  2
-    guestScore.textContent =guestCount
-}
-function guestThree() {
-    guestCount  +=  3
-    guestScore.textContent = guestCount
-}
-function defaultss() {
-    count = 0
-    guestScore.textContent = count
-    homeScore.textContent = count
-    homeCount = 0
-    guestCount = 0
-}
+// index.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    let homeScore = document.getElementById("home_score");
+    let guestScore = document.getElementById("guest_score");
+    let resetScore = document.getElementById("reset");
+    let homeCount = 0;
+    let guestCount = 0;
+
+    function homeOne() {
+        homeCount += 1;
+        homeScore.textContent = homeCount;
+    }
+
+    function homeTwo() {
+        homeCount += 2;
+        homeScore.textContent = homeCount;
+    }
+
+    function homeThree() {
+        homeCount += 3;
+        homeScore.textContent = homeCount;
+    }
+
+    function guestOne() {
+        guestCount += 1;
+        guestScore.textContent = guestCount;
+    }
+
+    function guestTwo() {
+        guestCount += 2;
+        guestScore.textContent = guestCount;
+    }
+
+    function guestThree() {
+        guestCount += 3;
+        guestScore.textContent = guestCount;
+    }
+
+    function defaultss() {
+        homeCount = 0;
+        guestCount = 0;
+        homeScore.textContent = homeCount;
+        guestScore.textContent = guestCount;
+    }
+});
