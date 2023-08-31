36f04878f41878b9c2cbe09ed0542d81c9cd2c28
Akpolo Ogagaoghene Prince <104951733+ogazboiz@users.noreply.github.com>
2023-08-31 09:29:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-let homeScore= document.getElementById("home_score") 
-let guestScore= document.getElementById("guest_score")
-let resetScore= document.getElementById("reset")
-let count = 0
-
-function homeOne(){
-    count  +=  1
-    homeScore.innerText= count
+let homeScore = document.getElementById("home_score") 
+let guestScore = document.getElementById("guest_score")
+let resetScore = document.getElementById("reset")
+let homeCount = 0
+let guestCount = 0
+function homeOne() {
+    homeCount   +=  1
+    homeScore.textContent= homeCount 
 }
-function homeTwo(){
-    count = count + 2
-    homeScore.textContent = count
+function homeTwo() {
+    homeCount   +=  2
+    homeScore.textContent = homeCount 
 }
-function homeThree(){
-    count = count + 3
-    homeScore.textContent = count
+function homeThree() {
+    homeCount   +=  3
+    homeScore.textContent = homeCount 
 }
-function guestOne(){
-    count = count + 1
-    guestScore.textContent = count
+function guestOne() {
+    guestCount  +=  1
+    guestScore.textContent = guestCount
 }
-function guestTwo(){
-    count = count + 2
-    guestScore.textContent = count
+function guestTwo() {
+    guestCount  +=  2
+    guestScore.textContent =guestCount
 }
-function guestThree(){
-    count = count + 3
-    guestScore.textContent = count
+function guestThree() {
+    guestCount  +=  3
+    guestScore.textContent = guestCount
 }
 function defaultss() {
     count = 0
     guestScore.textContent = count
     homeScore.textContent = count
+    homeCount = 0
+    guestCount = 0
 }
